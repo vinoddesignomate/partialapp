f201fb7e62e5a9b6db12141e9bab723d96d8b624
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2022-11-17 23:29:20 +0530

    create theme extensions

--- theme-app-extension/assets/cg_partial_pay.js
+++ theme-app-extension/assets/cg_partial_pay.js
@@ -1,44 +1,10 @@
-async function loadSomething() {
-    while (!window.hasOwnProperty("jQuery")) {
-        await new Promise(resolve => setTimeout(resolve, 100));
-    }
+var jQuery = $.noConflict(true);
 
-    jQuery(function () {
-        jQuery('body').on('click', '[name="checkout"], [name="goto_pp"], [name="goto_gc"]', function () {
-            var formIsValid = true;
-            var message = "Please inform us ........";
-            jQuery('[name^="attributes"], [name="note"]').filter('.required, [required="required"]').each(function () {
-                jQuery(this).removeClass('error');
-                if (formIsValid && jQuery(this).val() == '') {
-                    formIsValid = false;
-                    message = jQuery(this).attr('data-error') || message;
-                    jQuery(this).addClass('error');
-                }
-            });
-            if (formIsValid) {
-                jQuery(this).submit();
-            }
-            else {
-                alert(message);
-                return false;
-            }
-        });
-    });
+jQuery( document ).ready(function() {  // ERROR!  jQuery library hasn't been run yet, so we can't use jQuery to select the document!
 
-    $(window).on("beforeunload", function () {
-        var cartForm = jQuery('form[action="/cart"]');
-        if (cartForm.size()) {
-            var params = {
-                type: 'POST',
-                url: '/cart/update.js',
-                data: cartForm.serialize(),
-                dataType: 'json',
-                async: false
-            };
-            jQuery.ajax(params);
-        }
-    });
+    jQuery('#add-btn').on('click', function(event){
+     alert("clicked")
+    }) 
+  });
 
-}
 
-loadSomething();
