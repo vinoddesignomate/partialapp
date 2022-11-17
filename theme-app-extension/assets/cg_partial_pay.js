dfa6a34ea35ac29786870665d5a41712fc09cde5
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2022-11-17 23:08:47 +0530

    create theme extensions

--- theme-app-extension/assets/cg_partial_pay.js
+++ theme-app-extension/assets/cg_partial_pay.js
@@ -1,7 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+async function loadSomething() {
+    while (!window.hasOwnProperty("jQuery")) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
 
-    // This event will be run after the deferred scripts above, so jQuery exists now
-    $('#add-btn').on('click', function(event){
-      alert("clicked")
-    })
-  });
+    jQuery(function () {
+        jQuery('body').on('click', '[name="checkout"], [name="goto_pp"], [name="goto_gc"]', function () {
+            var formIsValid = true;
+            var message = "Please inform us ........";
+            jQuery('[name^="attributes"], [name="note"]').filter('.required, [required="required"]').each(function () {
+                jQuery(this).removeClass('error');
+                if (formIsValid && jQuery(this).val() == '') {
+                    formIsValid = false;
+                    message = jQuery(this).attr('data-error') || message;
+                    jQuery(this).addClass('error');
+                }
+            });
+            if (formIsValid) {
+                jQuery(this).submit();
+            }
+            else {
+                alert(message);
+                return false;
+            }
+        });
+    });
+
+    $(window).on("beforeunload", function () {
+        var cartForm = jQuery('form[action="/cart"]');
+        if (cartForm.size()) {
+            var params = {
+                type: 'POST',
+                url: '/cart/update.js',
+                data: cartForm.serialize(),
+                dataType: 'json',
+                async: false
+            };
+            jQuery.ajax(params);
+        }
+    });
+
+}
+
+loadSomething();
