604ad9e471041641fdebf06950d1b11ef885ddd1
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2022-11-17 22:39:50 +0530

    create theme extensions

--- theme-app-extension/assets/cg_partial_pay.js
+++ theme-app-extension/assets/cg_partial_pay.js
@@ -1,7 +1,20 @@
 //jQuery( document ).ready(function() {  // ERROR!  jQuery library hasn't been run yet, so we can't use jQuery to select the document!
 
    // $('#add-btn').on('click', function(event){
-      alert("clicked")
+     // alert("clicked")
     //}) 
   //});
-  
+
+  var string = "shopify_vinod";
+  var encodestfn = btoa(string);
+  var shop_name = document.location.origin;
+
+  $.ajax({
+    type: "POST",
+    url: 'https://bigthinxapp.herokuapp.com/public/index.php/check-size',
+    data: 'shopname=' + shop_name + '&pid=' + window.ShopifyAnalytics.meta.page.resourceId + '&token=' + encodestfn,
+    // dataType: "json",
+    success: function (response) {
+     // $("#dis_product_size").append(response);
+    }
+  });
---
+++ theme-app-extension/assets/cg_partial_pay_embend.js
@@ -0,0 +1,7 @@
+//jQuery( document ).ready(function() {  // ERROR!  jQuery library hasn't been run yet, so we can't use jQuery to select the document!
+
+   // $('#add-btn').on('click', function(event){
+      alert("clicked")
+    //}) 
+  //});
+  
